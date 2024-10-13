98d693d2d35ee8b546cbd83d8981671f35d83a23
xambroz <723625+xambroz@users.noreply.github.com>
2024-10-13 09:20:14 +0200

    add license text to the file

--- compatible_shortcuts.sh
+++ compatible_shortcuts.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+LICENSE="MIT"
+LICENSE-TEXT="MIT License
+
+Copyright (c) 2024 Michal Ambroz
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"
+
+# Compare current settings with default
 # gsettings list-recursively | grep -E 'keybindings|media-keys' > gnome_keybindings.txt
 
 
@@ -29,7 +54,6 @@ gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Super>spa
 # Workspace switching, win compatibility <Super><Control>Left/Right
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Primary><Alt>Left','<Super><Control>Left']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Primary><Alt>Right','<Super><Control>Right']"
-
 
 
 # Window placement
